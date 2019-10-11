11019e20c2f964605b602ded5b168c9c6c6461ec
Nima.Sadr <fatemeh.tozandehjany118@gmail.com>
2019-10-11 13:57:00 -0700

    Init Project

---
+++ MadPay724.ServicesLayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MadPay724.ServicesLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
